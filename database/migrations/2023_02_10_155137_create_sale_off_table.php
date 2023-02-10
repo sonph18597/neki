ea8c16d1b58e412fedf3270ff5eb17429f31fc2d
congdu28 <124913089+dutcph15015@users.noreply.github.com>
2023-02-10 22:56:27 +0700

    Add Table

---
+++ database/migrations/2023_02_10_155137_create_sale_off_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sale_off', function (Blueprint $table) {
+            $table->id();
+            $table->string('ten');
+            $table->string('time_start');
+            $table->string('time_end');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sale_off');
+    }
+};
