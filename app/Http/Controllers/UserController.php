dcc6795beeb4f8a9ddc6449495d021694a6d65d6
conmeowngungoc <101876056+conmeowngungoc@users.noreply.github.com>
2023-02-13 23:59:47 +0700

    user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,22 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Session;
+use Illuminate\Http\JsonResponse;
 
 class UserController extends Controller
 {
-    public function __construct()
-    {
-        $this->v=[];
-    }
-    public function index(Request $request){
-        $user = new User();
-        $this->v['title']="ABC";
-        $this->v['extParams']= $request->all();
-        $this->v['list']=$user->loadListWithPager();
-//        dd($this->v['list']);
-        return view("admin.user.index", $this->v);
+    public function getUser(){
+    $model = new User();
+    $user = $model->loadListWithPager();
+        return response()->json([
+            'result' => true,
+            'status_code' => JsonResponse::HTTP_OK,
+            'contents' => [
+                'entries' => $user
+            ]
+        ], JsonResponse::HTTP_OK);
     }
 }
