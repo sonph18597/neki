ced7b4e40f51c799a54e8b85f31f7b7923061ef7
minhnhat2611 <nhatnm2611@gmail.com>
2023-03-23 20:08:54 +0700

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,57 +22,5 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        $dataShoes = [
-            [
-                "id"=>1,
-                "id_prod_sale"=>1,
-                "name"=>"nike",
-                "list_img"=>"q",
-                "id_type"=>1,
-                "description"=>"abc",
-                "list_variant"=>"1",
-                "min_price"=>200000,
-                "max_price"=>400000
-            ],
-            [
-                "id"=>2,
-                "id_prod_sale"=>1,
-                "name"=>"nike",
-                "list_img"=>"q",
-                "id_type"=>1,
-                "description"=>"abc",
-                "list_variant"=>"1",
-                "min_price"=>200000,
-                "max_price"=>400000
-            ],
-        ];
-        $dataDiscountCode = [
-            [
-                "id"=>1,
-                "discount_code"=>"free123",
-                "exclude_prod"=>"1",
-                "include_prod"=>"1",
-                "condition_type"=>"1",
-"type_discount"=>"1",
-"discount_number"=>1,
-"limits"=>10,
-"time_start"=>27/1/2023,
-"time_end"=>27/10/2023,
-            ], [
-                "id"=>1,
-                "discount_code"=>"free123",
-                "exclude_prod"=>"1",
-                "include_prod"=>"1",
-                "condition_type"=>"1",
-"type_discount"=>"1",
-"discount_number"=>1,
-"limits"=>10,
-"time_start"=>27/1/2023,
-"time_end"=>27/10/2023,
-            ]
-        ];
-
-        DB::table('shoes')->insert($dataShoes);
-        DB::table('discount_code')->insert($dataDiscountCode);
     }
 }
