7c24a504cb9bfaf8c6a5ac6c76f57e7a627fa36d
conmeowngungoc <101876056+conmeowngungoc@users.noreply.github.com>
2023-03-30 21:03:43 +0700

    api/Loai

---
+++ app/Http/Controllers/LoaiController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\AddLoaiRequest;
+use App\Http\Requests\GetLoaiRequest;
+use App\Http\Requests\UpdateLoaiRequest;
+use App\Models\Loai;
+use Illuminate\Http\JsonResponse;
+
+class LoaiController extends Controller
+{
+    public function getAllLoai(GetLoaiRequest $request){
+        $model = new Loai();
+        $loai = $model->loadListWithPager($request->input());
+        return response()->json([
+            'result' => true,
+            'status_code' => JsonResponse::HTTP_OK,
+            'contents' => [
+                'entries' => $loai
+            ]
+        ], JsonResponse::HTTP_OK);
+    }
+
+    public function addLoai(AddLoaiRequest $request ){
+        $params = [];
+        $params['cols'] = $request->post();
+        unset( $params['cols']['_token']);
+        $loai = new Loai();
+        $loai->saveNew($params);
+        return response()->json([
+            'result' => true,
+            'status_code' => JsonResponse::HTTP_OK,
+            'contents' => [
+                'entries' => [
+                    'id' => true,
+                ]
+            ]
+        ], JsonResponse::HTTP_OK);
+    }
+
+    public function updateLoai($id, UpdateLoaiRequest $request ){
+        $params = [];
+        $params['cols'] = $request->post();
+        $params['cols']['id'] = $id;
+        unset( $params['cols']['_token']);
+        $loai = new Loai();
+        $loai->saveUpdate($params);
+        return response()->json([
+            'result' => true,
+            'status_code' => JsonResponse::HTTP_OK,
+            'contents' => [
+                'entries' => [
+                    'id' => $loai->id
+                ]
+            ]
+        ], JsonResponse::HTTP_OK);
+    }
+
+    public function getOneLoai($id){
+        $model = new Loai();
+        $loai = $model->loadOne($id);
+        return response()->json([
+            'result' => true,
+            'status_code' => JsonResponse::HTTP_OK,
+            'contents' => [
+                'entries' => [
+                    'loai' => $loai,
+                ]
+            ]
+        ], JsonResponse::HTTP_OK);
+    }
+    public function deleteLoai($id)
+    {
+        $loai = Loai::find($id);
+        if (!$loai) {
+            return response()->json(['error' => 'Loại sản phẩm này không tồn tại'], 404);
+        }
+        $loai->delete();
+        return response()->json([
+            'result' => true,
+            'status_code' => JsonResponse::HTTP_OK,
+            'contents' => [
+                'entries' => [
+                    'loai' => $loai,
+                ]
+            ]
+        ], JsonResponse::HTTP_OK);
+    }
+}
